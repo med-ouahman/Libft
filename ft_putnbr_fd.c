cfae509c6edadab281a369fb0acaeddaaf7a357a
Mohamed Ouahmane <147941749+med-ouahman@users.noreply.github.com>
2025-02-24 09:13:40 +0000

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,20 +14,5 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	unsigned int	nb;
-
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		nb = -n;
-	}
-	else
-		nb = n;
-	if (nb < 10)
-		ft_putchar_fd(nb + 48, fd);
-	else
-	{
-		ft_putnbr_fd(nb / 10, fd);
-		ft_putnbr_fd(nb % 10, fd);
-	}
+	write(fd, &n, sizeof(n));
 }
